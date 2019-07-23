e1e25c7ea05f4f90d7b98793592f0238679005f3
Kara Cavanaugh <karacav@msn.com>
2019-07-23 19:40:45 -0400

    Create ChapterEight.java

---
+++ ChapterEight.java
@@ -0,0 +1,12 @@
+public class ChapterEight {
+  public static void main(String[] args) {
+    Dog myDog = new Dog();
+    Cat myCat = new Cat();
+    Wolf myWolf = new Wolf();
+    myWolf.whoAmI();
+    myCat.speak();
+    myCat.whoAmI();
+    myDog.numberOfLegs();
+    myDog.whoAmI();
+  }
+}
